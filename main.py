fc1b309d342dcc520910c70db9b6878aa8957aba
NinaJ24 <162774340+NinaJ24@users.noreply.github.com>
2024-10-27 01:27:59 +0800

    Update and rename main to main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pinecone import Pinecone
 
 
 load_dotenv()
-OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
+
 PINECONE_API_KEY = st.secrets['PINECONE_API_KEY']
 
 pc = Pinecone(api_key=PINECONE_API_KEY)
